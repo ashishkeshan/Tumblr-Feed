a35c7a2677fab46ffa13e960dec77dc2ad8b93ec
Ashish Keshan <keshan@usc.edu>
2016-09-22 17:26:13 -0700

    first commit

---
+++ File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Tumblr Feed
+//
+//  Created by Ashish Keshan on 9/16/16.
+//  Copyright © 2016 Ashish Keshan. All rights reserved.
+//
+
+import Foundation
---
+++ Tumblr Feed/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  Tumblr Feed
+//
+//  Created by Ashish Keshan on 9/16/16.
+//  Copyright © 2016 Ashish Keshan. All rights reserved.
+//
+
+import Cocoa
+
+class PhotoCell: UITableViewCell {
+
+}
